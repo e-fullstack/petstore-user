0843591cbd2678afc2cd179530c3664d45b776bc
samitkumarpatel <samitkumarpatel@gmail.com>
2022-08-16 22:17:01 +0200

    first commit

---
+++ src/main/java/com/samitkumarpatel/petstore/user/PetstoreUserApplication.java
@@ -0,0 +1,13 @@
+package com.samitkumarpatel.petstore.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samitkumarpatel/petstore/user/PetstoreUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samitkumarpatel.petstore.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
